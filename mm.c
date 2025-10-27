1ee066a40e884ff63f0709c9afd71b05e4e44396
sunmok <rlfhr9191@gmail.com>
2025-10-27 23:20:42 +0900

    feat:explicit_LIFO_first_fit

--- mm.c
+++ mm.c
@@ -16,6 +16,9 @@
 #include <string.h>
 
 #include "mm.h"
+
+#include <limits.h>
+
 #include "memlib.h"
 
 /*********************************************************
@@ -70,19 +73,28 @@ team_t team = {
 #define NEXT_BLKP(bp) ((char *)(bp) + GET_SIZE(((char *)(bp) - WSIZE)))
 #define PREV_BLKP(bp) ((char *)(bp) - GET_SIZE(((char *)(bp) - DSIZE)))
 
-typedef void* (*fit_policy_t)(size_t asize);
+#define PREDP(bp) (*(void **)(bp))
+#define SUCCP(bp) (*(void **)(bp + WSIZE))
+
+typedef void * (*fit_policy_t)(size_t asize);
+
+static char *free_listp;
 
 static char *heap_listp;
 
-static void *coalesce(void *bp);
+static void *coalesce_for_first_fit(void *bp);
 
 static void *extend_heap(size_t words);
 
 static void *find_fit(size_t asize, fit_policy_t fit_policy);
 
-static void * first_fit(size_t asize);
+static void *first_fit(size_t asize);
 
-static void place(void *bp, size_t asize);
+static void place_for_first_fit(void *bp, size_t asize);
+
+static void list_add(void *bp);
+
+static void list_remove(void *bp);
 
 /*
  * mm_init - initialize the malloc package.
@@ -91,13 +103,14 @@ int mm_init(void) {
     if ((heap_listp = mem_sbrk(4 * WSIZE)) == (void *) -1)
         return -1;
 
-    PUT(heap_listp, 0); /* Alignment padding */
+    PUT(heap_listp, 0);                         /* Alignment padding */
     PUT(heap_listp + (1*WSIZE), PACK(DSIZE, 1)); /* Prologue header */
     PUT(heap_listp + (2*WSIZE), PACK(DSIZE, 1)); /* Prologue footer */
-    PUT(heap_listp + (3*WSIZE), PACK(0, 1)); /* Epilogue header */
-    heap_listp += (2 * WSIZE);
+    PUT(heap_listp + (3*WSIZE), PACK(0, 1));     /* Epilogue header */
+    heap_listp += (2 * WSIZE); // heap_listp를 Prologue 페이로드로 이동
 
-    /* Extend the empty heap with a free block of CHUNKSIZE bytes */
+    free_listp = NULL;
+
     if (extend_heap(CHUNKSIZE / WSIZE) == NULL)
         return -1;
     return 0;
@@ -124,7 +137,7 @@ void *mm_malloc(size_t size) {
 
     /* Search the free list for a fit */
     if ((bp = find_fit(asize, first_fit)) != NULL) {
-        place(bp, asize);
+        place_for_first_fit(bp, asize);
         return bp;
     }
 
@@ -132,7 +145,7 @@ void *mm_malloc(size_t size) {
     extendsize = MAX(asize, CHUNKSIZE);
     if ((bp = extend_heap(extendsize / WSIZE)) == NULL)
         return NULL;
-    place(bp, asize);
+    place_for_first_fit(bp, asize);
     return bp;
 }
 
@@ -144,7 +157,7 @@ void mm_free(void *bp) {
 
     PUT(HDRP(bp), PACK(size, 0));
     PUT(FTRP(bp), PACK(size, 0));
-    coalesce(bp);
+    coalesce_for_first_fit(bp);
 }
 
 /*
@@ -177,51 +190,82 @@ static void *extend_heap(size_t words) {
         return NULL;
 
     /* Initialize free block header/footer and the epilogue header */
+
     PUT(HDRP(bp), PACK(size, 0)); /* Free block header */
     PUT(FTRP(bp), PACK(size, 0)); /* Free block footer */
     PUT(HDRP(NEXT_BLKP(bp)), PACK(0, 1)); /* New epilogue header */
 
     /* Coalesce if the previous block was free */
-    return coalesce(bp);
+    return coalesce_for_first_fit(bp);
 }
 
-static void *coalesce(void *bp) {
+static void list_add(void *bp) {
+    PREDP(bp) = NULL;
+    SUCCP(bp) = free_listp;
+    if (free_listp != NULL) {
+        PREDP(free_listp) = bp;
+    }
+
+    free_listp = bp;
+}
+
+static void list_remove(void *bp) {
+    void *pred_bp = PREDP(bp);
+    void *succ_bp = SUCCP(bp);
+
+    if (pred_bp == NULL && succ_bp == NULL) {
+        free_listp = NULL;
+    } else if (pred_bp == NULL) {
+        PREDP(succ_bp) = NULL;
+        free_listp = succ_bp;
+    } else if (succ_bp == NULL) {
+        SUCCP(pred_bp) = NULL;
+    } else {
+        SUCCP(pred_bp) = succ_bp;
+        PREDP(succ_bp) = pred_bp;
+    }
+}
+
+static void *coalesce_for_first_fit(void *bp) {
     size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
     size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
     size_t size = GET_SIZE(HDRP(bp));
 
     if (prev_alloc && next_alloc) {
         /* Case 1 */
-        return bp;
-    }
-
-    if (prev_alloc && !next_alloc) {
+    } else if (prev_alloc && !next_alloc) {
         /* Case 2 */
+        list_remove(NEXT_BLKP(bp));
         size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
         PUT(HDRP(bp), PACK(size, 0));
         PUT(FTRP(bp), PACK(size, 0));
     } else if (!prev_alloc && next_alloc) {
         /* Case 3 */
+        list_remove(PREV_BLKP(bp));
         size += GET_SIZE(HDRP(PREV_BLKP(bp)));
         PUT(FTRP(bp), PACK(size, 0));
         PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
         bp = PREV_BLKP(bp);
     } else {
         /* Case 4 */
+        list_remove(PREV_BLKP(bp));
+        list_remove(NEXT_BLKP(bp));
         size += GET_SIZE(HDRP(PREV_BLKP(bp))) +
                 GET_SIZE(FTRP(NEXT_BLKP(bp)));
         PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
         PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0));
         bp = PREV_BLKP(bp);
     }
+
+    list_add(bp);
     return bp;
 }
 
-static void * first_fit(size_t asize) {
+static void *first_fit(size_t asize) {
     void *bp;
 
-    for (bp = heap_listp; GET_SIZE(HDRP(bp)) > 0; bp = NEXT_BLKP(bp)) {
-        if (!GET_ALLOC(HDRP(bp)) && (asize <= GET_SIZE(HDRP(bp)))) {
+    for (bp = free_listp; bp != NULL; bp = SUCCP(bp)){
+        if (asize <= GET_SIZE(HDRP(bp))) {
             return bp;
         }
     }
@@ -232,8 +276,9 @@ static void *find_fit(size_t asize, fit_policy_t fit_policy) {
     return fit_policy(asize);
 }
 
-static void place(void *bp, size_t asize) {
+static void place_for_first_fit(void *bp, size_t asize) {
     size_t csize = GET_SIZE(HDRP(bp));
+    list_remove(bp);
 
     if ((csize - asize) >= (2 * DSIZE)) {
         PUT(HDRP(bp), PACK(asize, 1));
@@ -241,6 +286,7 @@ static void place(void *bp, size_t asize) {
         bp = NEXT_BLKP(bp);
         PUT(HDRP(bp), PACK(csize-asize, 0));
         PUT(FTRP(bp), PACK(csize-asize, 0));
+        list_add(bp);
     } else {
         PUT(HDRP(bp), PACK(csize, 1));
         PUT(FTRP(bp), PACK(csize, 1));
